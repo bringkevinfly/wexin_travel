9f81b24acfdbbdef9ea3898489d539245be73fe4
Yan Kevin <bringmefly@Yans-MacBook-Pro.local>
2014-04-20 11:02:17 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes');
+var users = require('./routes/user');
+
+var app = express();
+
+var utils = require('express/node_modules/connect/lib/utils'), xml2js = require('xml2js');
+
+function xmlBodyParser(req, res, next) {
+    if (req._body) return next();
+    req.body = req.body || {};
+
+    // ignore GET
+    if ('GET' == req.method || 'HEAD' == req.method) return next();
+
+    // check Content-Type
+    if ('text/xml' != utils.mime(req)) return next();
+
+    // flag as parsed
+    req._body = true;
+
+    // parse
+    var buf = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk){ buf += chunk });
+    req.on('end', function(){
+        var parseString = xml2js.parseString;
+        parseString(buf, function(err, json) {
+            if (err) {
+                err.status = 400;
+                next(err);
+            } else {
+                req.body = json;
+                next();
+            }
+        });
+    });
+};
+
+app.set('port', process.env.PORT || 3000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(xmlBodyParser);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+app.get('/', routes.index);
+app.get('/users', users.list);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+http.createServer(app).listen(app.get('port'),function(){
+    console.log('Express server listening to port '+ app.get('port'));
+});
+
+//module.exports = app;
---
+++ server.js
@@ -0,0 +1,5 @@
+/**
+ * Created by bringmefly on 14-4-20.
+ */
+var weixin_travel_app = require('./app');
+weixin_travel_app();
